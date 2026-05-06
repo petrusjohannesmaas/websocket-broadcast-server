c7da00af416365b6c0267f9abdedbbb1b7e86bda
PJ Maas <pjmaasdev@gmail.com>
2026-05-06 21:29:04 +0200

    fix: connect flag parsing error

--- main.go
+++ main.go
@@ -9,16 +9,17 @@ import (
 )
 
 func main() {
-	port := flag.Int("port", 8080, "port to listen on")
-	host := flag.String("host", "localhost", "host to connect to")
-	flag.Parse()
-
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s <start|connect>\n", os.Args[0])
 		os.Exit(1)
 	}
 
 	command := os.Args[1]
+	os.Args = append(os.Args[:1], os.Args[2:]...)
+
+	port := flag.Int("port", 8080, "port to listen on")
+	host := flag.String("host", "localhost", "host to connect to")
+	flag.Parse()
 
 	if command == "start" {
 		server := NewServer(*port)
@@ -28,8 +29,7 @@ func main() {
 		reader := bufio.NewReader(os.Stdin)
 		url := fmt.Sprintf("ws://%s:%d/ws", *host, *port)
 		client := NewClient(url, "", reader)
-		err := client.Connect()
-		if err != nil {
+		if err := client.Connect(); err != nil {
 			log.Fatalf("Connection failed: %v", err)
 		}
 	} else {
